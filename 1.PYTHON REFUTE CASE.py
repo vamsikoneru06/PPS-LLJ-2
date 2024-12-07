979668344af1bbaaa2edab4944dc664bb4fb0daa
vamsikoneru06 <mk7116@srmist.edu.in>
2024-12-07 22:51:32 +0530

    python1.py

---
+++ 1.PYTHON REFUTE CASE.py
@@ -0,0 +1,8 @@
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+if num2 == 0:
+    print("Division by zero is not allowed.")
+else:
+    result = num1 / num2
+    print("Result:", result)
